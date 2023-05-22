683724313a9c87ff282afaa5bbec54db843317ba
Andy Omolo <asum505@gmail.com>
2023-05-22 02:46:29 -0700

    m

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,15 @@ int main(void)
 {
 	int ch;
 	for (ch = 0; ch <= 9; ch++)
-
 	{
 		putchar(ch + '0');
+
 			if (ch != 9)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 	}
+	putchar('\n');
 	return (0);
 }
