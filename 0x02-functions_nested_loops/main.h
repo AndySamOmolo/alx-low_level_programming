52800d82057fb6490149072a607af36ec0acd831
Andy Omolo <asum505@gmail.com>
2023-05-23 04:45:30 -0700

    message

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,1 @@
-#include <stdio.h>
-
 void print_alphabet(void);
-void print_alphabet_x10(void)
-{
-	int i;
-        for(i = 0; i <=10; i++)
-	{
-        	char ch;
-
-                for (ch = 'a'; ch <= 'z'; ch++)
-                {
-                        putchar(ch);
-                }
-        }
-        putchar('\n');
-        return (0);
-}
