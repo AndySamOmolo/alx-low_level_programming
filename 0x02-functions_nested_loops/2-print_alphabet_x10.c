11e5267871707b96ada5b75aa36158c0a2672114
Andy Omolo <asum505@gmail.com>
2023-05-23 04:33:58 -0700

    message

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,15 +11,3 @@ int main(void)
 	print_alphabet_x10();
 	return (0);
 }
-
-void print_alphabet_x10(void)
-{
-    for (int i = 0; i <= 10; i++)
-    {
-        for (char ch = 'a'; ch <= 'z'; ch++)
-        {
-            putchar(ch);
-        }
-        putchar('\n');
-    }
-}
